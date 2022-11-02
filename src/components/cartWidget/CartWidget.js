054b5db21dd6219a7022401c6571b6541c11f07e
juan ignacio sacco <junchisacco@gmail.com>
2022-11-02 13:33:47 -0300

    Solucion en cartWiget

--- src/components/cartWidget/CartWidget.js
+++ src/components/cartWidget/CartWidget.js
@@ -2,15 +2,20 @@ import './cartWidget.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 import { CartContext } from '../../contextos/CartContext'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 
 const CartWidget = () => {
 
     const {totalItems} = useContext(CartContext)
+    const [totalItemsCart, setTotalItmesCart] = useState(0)
+
+    useEffect(() => {
+        totalItems > 0 && setTotalItmesCart(totalItems)
+    }, [totalItems])
 
     return (
         <div>
-            {totalItems > 0 && totalItems}
+            {totalItemsCart !== 0 && totalItemsCart}
             <i className='icono'><FontAwesomeIcon icon={faCartShopping} /></i>
         </div>
     )
